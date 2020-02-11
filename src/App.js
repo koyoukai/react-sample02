ae1fa18edceccdcef7027f2d7e98b768af8b21cd
koyo <kakiage@gmail.com>
2020-02-11 17:26:18 +0900

    add library redux reac-redux etc

--- src/App.js
+++ src/App.js
@@ -1,35 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import './App.css';
-
-const initProfile = {
-  followers: null,
-  publicRepos: null
-}
 
 function App() {
-  const [profile, setProfile] = useState(initProfile)
-  
-  useEffect(() => {
-    getProfile()
-  }, [])
-
-  async function getProfile() {
-    const response = await fetch('https://api.github.com/users/gitmil')
-    const json = await response.json()
-
-    setProfile({
-      followers: json.followers,
-      publicRepos: json.public_repos
-    })
-  }
-
   return (
-    <div className="App">
-      <h2>fetch data</h2>
-
-      followers: {profile.followers} <br />
-      public_repos: {profile.publicRepos}
-      
+    <div className="App">      
     </div>
   );
 }
